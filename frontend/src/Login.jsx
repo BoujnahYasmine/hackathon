e99754ea14013d711a6408516528d3cd8c65917e
Yasmine Boujnah <132377962+BoujnahYasmine@users.noreply.github.com>
2025-07-04 12:42:57 +0100

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -13,7 +13,7 @@ const Login = ({setIsAuthenticated}) => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/login", {
+      const res = await axios.post("https://hackathon-44is.onrender.com/api/login", {
         email,
         password,
       });
